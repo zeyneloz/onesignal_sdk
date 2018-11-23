d35ce3724ef95a54e46dc2f9321ab2579354f07f
zeyneloz <zeynel.ozdmir@gmail.com>
2018-11-23 11:52:22 +0100

    Bump version to v0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='onesignal_sdk',
 
-    version='0.1.1',
+    version='0.2.0',
 
     description='A Python wrapper around the OneSignal API Edit Add topics',
     long_description=long_description,
